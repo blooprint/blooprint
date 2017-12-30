f01828e49756eacc82fe3b101c6613678a6d9dea
ddaaggeett <david.g.daggett@gmail.com>
2017-12-30 01:08:31 -0500

    loading style

--- source/js/components/Restaurant/dumb/Loading.jsx
+++ source/js/components/Restaurant/dumb/Loading.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import MediaQuery from 'react-responsive'
+
+var blooprintImage = require('../../../../assets/img/blooprint.png')
 
 const Loading = (props) => {
     return (
-        <div>
-            Loading ...
+        <div id="loader">
+            <p className="center_menu_loading">loading <img className="center_menu_loading" id="loading_tm" src={blooprintImage} alt="blooprint" /></p>
+            <p className="center_menu_loading">menu listing</p>
+            <p id="loading_cursor" className="center_menu_loading">....</p>
         </div>
     )
 }
